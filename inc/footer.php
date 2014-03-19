94240c4322b9a4c6118379851b5d2cc151e5c8e0
raystation <rayuen@gmail.com>
2014-03-19 11:17:57 -0700

    added in php includes

---
+++ inc/footer.php
@@ -0,0 +1,31 @@
+<!-- FOOTER 
+	====================================== -->		
+
+	<div class="full footer">
+		<div class="container add-padding">
+			<div class="four columns alpha add-bottom">
+				<h3 class="footer" id="close-line">Suspended<br/>Belief<br/>Studios</h3>
+			</div>
+			
+			<div class="four columns">
+				<h5 class="footer">Let's talk</h5>
+				<p><a href="mailto:jeremy@suspendedbeliefgames.com" target="_top">Email us</a>
+			</div>
+			
+			<div class="four columns">
+				<h5 class="footer">Facebook</h5>
+				<p><a href="https://www.facebook.com/SuspendedBeliefStudios">Suspended Belief Studios</a></p>
+			</div>
+			
+			<div class="four columns">
+				<h5 class="footer">Twitter</h5>
+				<p><a href="http://www.twitter.com/Storyingames">Storyingames</a></p>
+			</div>
+			
+		</div>
+	</div>
+
+<!-- End Document
+================================================== -->
+</body>
+</html>
---
+++ inc/header.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<!--[if lt IE 7 ]><html class="ie ie6" lang="en"> <![endif]-->
+<!--[if IE 7 ]><html class="ie ie7" lang="en"> <![endif]-->
+<!--[if IE 8 ]><html class="ie ie8" lang="en"> <![endif]-->
+<!--[if (gte IE 9)|!(IE)]><!--><html lang="en"> <!--<![endif]-->
+<head>
+
+	<!-- Basic Page Needs
+  ================================================== -->
+	<meta charset="utf-8">
+	<title><?php echo $title ;?></title>
+	<meta name="description" content="">
+	<meta name="author" content="">
+
+	<!-- Mobile Specific Metas
+  ================================================== -->
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+	<!-- CSS
+  ================================================== -->
+	<link rel="stylesheet" href="/sbstudios/css/base.css">
+	<link rel="stylesheet" href="/sbstudios/css/skeleton.css">
+	<link rel="stylesheet" href="/sbstudios/css/layout.css">
+	<link rel="stylesheet" href="/sbstudios/css/style.css">
+	
+	<script src="http://code.jquery.com/jquery-latest.js" type="text/javascript"></script>
+	<script src="js/smoothScroll.js" type="text/javascript"></script>
+
+	<!--[if lt IE 9]>
+		<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+	<![endif]-->
+
+	<!-- Favicons
+	================================================== -->
+	<link rel="shortcut icon" href="img/favicon.ico">
+	<link rel="apple-touch-icon" href="img/apple-touch-icon.png">
+	<link rel="apple-touch-icon" sizes="72x72" href="img/apple-touch-icon-72x72.png">
+	<link rel="apple-touch-icon" sizes="114x114" href="img/apple-touch-icon-114x114.png">
+	
+</head>
+<body>
+
+	<!--header-->
+	<div class="container add-bottom">
+		<div class="sixteen columns center logocontainer">
+			<a href="<?php echo $homelink ;?>"><img src="img/sb_logo.png" class="scale-with-grid"/></a>
+			<hr class="half-bottom">
+			<!-- <p class="half-bottom"><a href="projects.html">projects</a> | services | <a href="us.html">about us</a> | news</p> -->
+			
+		</div>
+	</div>
