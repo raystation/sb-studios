681152afd43dea05919171b04d1e44123a85865a
raystation <rayuen@gmail.com>
2014-04-02 17:38:54 -0700

    added the 3 page

--- footer.php
+++ footer.php
@@ -4,7 +4,7 @@
 	<div class="full footer">
 		<div class="container add-padding">
 			<div class="four columns alpha add-bottom">
-				<h3 class="footer" id="close-line">Suspended<br/>Belief<br/>Studios</h3>
+				<h3 class="footer" id="close-line"><a href="index.php">Suspended<br/>Belief<br/>Studios</h3></a>
 			</div>
 			
 			<div class="four columns">
@@ -13,13 +13,19 @@
 			</div>
 			
 			<div class="four columns">
-				<h5 class="footer">Facebook</h5>
-				<p><a href="https://www.facebook.com/SuspendedBeliefStudios">Suspended Belief Studios</a></p>
+				<h5 class="footer">Games</h5>
+				<p>
+					<a href="indiginauts.php">Indiginauts</a><br/>
+					<a href="the3.php">The 3</a>
+				</p>				
 			</div>
 			
 			<div class="four columns">
-				<h5 class="footer">Twitter</h5>
-				<p><a href="http://www.twitter.com/Storyingames">Storyingames</a></p>
+				<h5 class="footer">Social</h5>
+				<p>
+					<a href="https://www.facebook.com/SuspendedBeliefStudios">Facebook</a><br/>
+					<a href="http://www.twitter.com/Storyingames">Twitter</a>
+				</p>
 			</div>
 			
 		</div>
