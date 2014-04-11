ba9d868332181ff233a08122b4d5266b100d69c4
raystation <rayuen@gmail.com>
2014-04-11 13:53:09 -0700

    added in link to demo

--- header.php
+++ header.php
@@ -17,7 +17,8 @@
 
 	<!-- Mobile Specific Metas
   ================================================== -->
-	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+	<meta name="viewport" content="width=device-width, height=device-height, initial-scale=1, maximum-scale=1, user-scalable=no" />
+	<meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
 
 	<!-- CSS
   ================================================== -->
